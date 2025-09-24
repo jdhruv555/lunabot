a5efde9b692a33741f4950d22508b780e49813e3
Dhruv <135132021+jdhruv555@users.noreply.github.com>
2025-09-24 16:14:08 +0530

    fix(build): correct setup.py for base and perception

--- src/lunabot_perception/setup.py
+++ src/lunabot_perception/setup.py
@@ -1,19 +1,24 @@
 from setuptools import setup
 
-package_name = lunabot_perception
+package_name = 'lunabot_perception'
 
 setup(
     name=package_name,
-    version=0.1.0,
+    version='0.1.0',
     packages=[package_name],
     data_files=[
-        (share/ament_index/resource_index/packages, [resource/ + package_name]),
-        (share/ + package_name, [package.xml]),
-        (share/ + package_name + /launch, [launch/perception.launch.py]),
-        (share/ + package_name + /config, [config/perception.yaml]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/perception.launch.py']),
+        ('share/' + package_name + '/config', ['config/perception.yaml']),
     ],
-    install_requires=[setuptools],
+    install_requires=['setuptools'],
     zip_safe=True,
     entry_points={
-        console_scripts: [
-            lidar_filter
+        'console_scripts': [
+            'lidar_filter = lunabot_perception.lidar_filter:main',
+            'depth_to_obstacles = lunabot_perception.depth_to_obstacles:main',
+            'depth_to_scan = lunabot_perception.depth_to_scan:main',
+        ],
+    },
+)
